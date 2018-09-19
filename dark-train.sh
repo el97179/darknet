1066dedf937f3883e486c8772947f5f74370dd18
Ubuntu <tanman@AIML.5z0synhjo3eujch1q0yzoogkdf.bx.internal.cloudapp.net>
2018-09-19 12:00:40 +0000

    Changed script file to train for single class 'weapon'
    Committer: tanman <t.athanasiadis@tadaweb.com>

--- dark-train.sh
+++ dark-train.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd ~/dev/darknet
-./darknet detector train data/weapons/3-weapons.data cfg/yolov3-3-weapons.cfg models/darknet53.conv.74 -dont_show |& tee /dev/stderr 2> /tmp/darknet.log | grep --line-buffered images > loss.txt
+./darknet detector train data/weapons/weapon.data cfg/yolov3-weapon.cfg models/darknet53.conv.74 -dont_show |& tee /dev/stderr 2> /tmp/darknet.log | grep --line-buffered images > loss.log
